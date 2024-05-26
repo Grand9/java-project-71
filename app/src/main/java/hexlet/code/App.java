bc82e0a2ac9e5110383177aec5f7bf474ff67c3e
Arlan <Arlan110022@Gmail.com>
2024-05-26 14:34:57 +0500

    picocli help added

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,29 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 4.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Display this help message and exit.")
+    private boolean helpRequested;
+
+    @Option(names = {"-v", "--version"}, versionHelp = true, description = "Display version info and exit.")
+    private boolean versionRequested;
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() {
+        if(!helpRequested && !versionRequested) {
+            System.out.println("Hello World!");
+        }
+        return null;
     }
 }
