d35a59ceb7468f04f73ffc369089398cbbd84c54
Arlan <Arlan110022@Gmail.com>
2024-05-27 14:16:39 +0500

    jackson-databind added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,9 @@ repositories {
 }
 
 dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.16.0")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.16.0")
     implementation("info.picocli:picocli:4.7.6")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
