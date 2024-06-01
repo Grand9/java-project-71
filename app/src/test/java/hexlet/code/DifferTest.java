e83cb795162fbd8e3b7f968b4231f51ad374f8d8
Arlan <Arlan110022@Gmail.com>
2024-06-01 15:28:22 +0500

    work with yaml added

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,8 +1,10 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.Test;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DifferTest {
@@ -11,6 +13,14 @@ public class DifferTest {
     public void testGenerateJsonComparison() throws Exception {
         String expected = new String(Files.readAllBytes(Paths.get("src/test/resources/expected1.yaml"))).trim();
         String result = Differ.generate("src/test/resources/file1.json", "src/test/resources/file2.json").trim();
+
+        assertThat(result).isEqualToIgnoringNewLines(expected);
+    }
+
+    @Test
+    public void testGenerateYamlComparison() throws Exception {
+        String expected = new String(Files.readAllBytes(Paths.get("src/test/resources/expected1.yaml"))).trim();
+        String result = Differ.generate("src/test/resources/file1.yaml", "src/test/resources/file2.yaml").trim();
 
         assertThat(result).isEqualToIgnoringNewLines(expected);
     }
