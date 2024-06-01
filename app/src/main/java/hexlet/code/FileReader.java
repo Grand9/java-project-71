aaacf948dc55e390a7285e4777d2cf16d725a197
Arlan <Arlan110022@Gmail.com>
2024-06-01 15:28:50 +0500

    minor change

--- app/src/main/java/hexlet/code/FileReader.java
+++ app/src/main/java/hexlet/code/FileReader.java
@@ -5,7 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class FileReader {
-
     public static String readFile(String filePath) throws IOException {
         return new String(Files.readAllBytes(Paths.get(filePath)));
     }
