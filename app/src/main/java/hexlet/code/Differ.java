0b1a7c1e8506b005b66c49b69471fd77e04732e5
Arlan <Arlan110022@Gmail.com>
2024-06-01 15:28:00 +0500

    work with yaml added

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,7 +1,6 @@
 package hexlet.code;
 
 import com.fasterxml.jackson.databind.JsonNode;
-
 import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
@@ -13,17 +12,30 @@ public class Differ {
     public static String generate(String filePath1, String filePath2) throws IOException {
         String content1 = FileReader.readFile(filePath1);
         String content2 = FileReader.readFile(filePath2);
-
-        JsonNode tree1 = JsonParser.parse(content1);
-        JsonNode tree2 = JsonParser.parse(content2);
-
-        return generate(tree1, tree2);
+        JsonNode tree1 = parseContent(content1, getFileExtension(filePath1));
+        JsonNode tree2 = parseContent(content2, getFileExtension(filePath2));
+        return generateDiff(tree1, tree2);
     }
 
-    private static String generate(JsonNode tree1, JsonNode tree2) {
+    private static JsonNode parseContent(String content, String fileType) throws IOException {
+        switch (fileType) {
+            case "json":
+                return JsonParser.parse(content);
+            case "yaml":
+            case "yml":
+                return YamlConverter.parse(content);
+            default:
+                throw new IllegalArgumentException("Unsupported file type: " + fileType);
+        }
+    }
+
+    private static String getFileExtension(String filePath) {
+        return filePath.substring(filePath.lastIndexOf('.') + 1).toLowerCase();
+    }
+
+    private static String generateDiff(JsonNode tree1, JsonNode tree2) {
         Map<String, JsonNode> map1 = toMap(tree1);
         Map<String, JsonNode> map2 = toMap(tree2);
-
         Set<String> allKeys = new TreeSet<>(map1.keySet());
         allKeys.addAll(map2.keySet());
 
@@ -48,7 +60,6 @@ public class Differ {
         }
 
         result.append("}");
-
         return result.toString();
     }
 
