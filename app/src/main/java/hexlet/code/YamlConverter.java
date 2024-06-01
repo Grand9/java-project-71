5632f5f6d85cae1e756597dc46a7932a769ad08e
Arlan <Arlan110022@Gmail.com>
2024-06-01 15:29:16 +0500

    work with yaml added

--- app/src/main/java/hexlet/code/YamlConverter.java
+++ app/src/main/java/hexlet/code/YamlConverter.java
@@ -1,15 +1,10 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-
+import com.fasterxml.jackson.databind.JsonNode;
 import java.io.IOException;
 
 public class YamlConverter {
-    public static String convertYamlToJson(String yamlString) throws IOException {
-        ObjectMapper yamlReader = new ObjectMapper(new YAMLFactory());
-        Object obj = yamlReader.readValue(yamlString, Object.class);
-        ObjectMapper jsonWriter = new ObjectMapper();
-        return jsonWriter.writeValueAsString(obj);
+    public static JsonNode parse(String content) throws IOException {
+        return JsonParser.parse(content);
     }
 }
