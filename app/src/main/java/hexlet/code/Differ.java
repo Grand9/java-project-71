163c2b19487a97154638cb22bcd8a0ed884e0610
Arlan <Arlan110022@Gmail.com>
2024-06-10 12:51:26 +0500

    create tree, separate class

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -14,8 +14,8 @@ public class Differ {
     }
 
     public static String generate(String file1, String file2, String format) throws Exception {
-        Map<String, Object> dataFile1 = JsonParser.parsing(file1);
-        Map<String, Object> dataFile2 = JsonParser.parsing(file2);
+        Map<String, Object> dataFile1 = Parser.parsing(file1);
+        Map<String, Object> dataFile2 = Parser.parsing(file2);
         List<Map<String, Object>> diff = calculateDiff(dataFile1, dataFile2);
         return Formatter.format(diff, format);
     }
@@ -28,30 +28,9 @@ public class Differ {
         List<Map<String, Object>> diffList = new ArrayList<>();
 
         for (String key : allKeys) {
-            Map<String, Object> diff = new HashMap<>();
-            diff.put("key", key);
-
-            if (dataFile1.containsKey(key) && dataFile2.containsKey(key)) {
-                if (Objects.equals(dataFile1.get(key), dataFile2.get(key))) {
-                    diff.put("type of change", "unchanged");
-                    diff.put("oldValue", dataFile1.get(key));
-                    diff.put("newValue", dataFile1.get(key));
-                } else {
-                    diff.put("type of change", "change");
-                    diff.put("oldValue", dataFile1.get(key));
-                    diff.put("newValue", dataFile2.get(key));
-                }
-            } else if (dataFile1.containsKey(key)) {
-                diff.put("type of change", "delete");
-                diff.put("oldValue", dataFile1.get(key));
-            } else {
-                diff.put("type of change", "add");
-                diff.put("newValue", dataFile2.get(key));
-            }
-
+            Map<String, Object> diff = Tree.buildDiff(key, dataFile1, dataFile2);
             diffList.add(diff);
         }
-
         return diffList;
     }
 }
---
+++ app/src/main/java/hexlet/code/Tree.java
@@ -0,0 +1,31 @@
+package hexlet.code;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Tree {
+    public static Map<String, Object> buildDiff(String key, Map<String, Object> dataFile1, Map<String, Object> dataFile2) {
+        Map<String, Object> diff = new HashMap<>();
+        diff.put("key", key);
+
+        if (dataFile1.containsKey(key) && dataFile2.containsKey(key)) {
+            if (Objects.equals(dataFile1.get(key), dataFile2.get(key))) {
+                diff.put("type of change", "unchanged");
+                diff.put("oldValue", dataFile1.get(key));
+                diff.put("newValue", dataFile1.get(key));
+            } else {
+                diff.put("type of change", "change");
+                diff.put("oldValue", dataFile1.get(key));
+                diff.put("newValue", dataFile2.get(key));
+            }
+        } else if (dataFile1.containsKey(key)) {
+            diff.put("type of change", "delete");
+            diff.put("oldValue", dataFile1.get(key));
+        } else {
+            diff.put("type of change", "add");
+            diff.put("newValue", dataFile2.get(key));
+        }
+        return diff;
+    }
+}
