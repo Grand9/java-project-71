4706fe108b74d629dfe119f0df33300407c8ac26
Arlan <Arlan110022@Gmail.com>
2024-06-13 13:44:58 +0500

    javadoc

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -3,6 +3,13 @@ package hexlet.code;
 import java.util.Map;
 
 public class Parser {
+    /**
+     * Parses the given content and returns a map representation.
+     *
+     * @param content The content to parse.
+     * @return A map representation of the parsed content.
+     * @throws Exception If an error occurs during parsing.
+     */
     Map<String, Object> parse(String content) throws Exception {
         return null;
     }
